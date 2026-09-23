10f4f3dc3129f000ee25171270cca9b3e329b99f
nothatcher-creator <nothatcher@gmail.com>
2026-09-23 20:11:48 -0300

    roomforge: bump M2 build overlay

---
+++ roomforge_m2_overlay/app/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.roomforge.gameify"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.roomforge.gameify"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 200
+        versionName = "0.2.0-m2"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("com.google.ar:core:1.56.0")
+}
